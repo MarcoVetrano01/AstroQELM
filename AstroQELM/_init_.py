a4ab345444326a484fb11f739bac674d52fd492e
Marco <marco.vetrano01@gmail.com>
2025-12-05 16:10:36 +0100

    modified init file to add names for functions in QELM module

--- AstroQELM/_init_.py
+++ AstroQELM/_init_.py
@@ -1,6 +1,6 @@
-from .utils import *
-from .preprocessing import *
-from .QELM import *
+from .utils import data_from_h5, data_from_txt
+from .preprocessing import spectra_normalization, add_zeros, JWST_interpolation, Bl, tran_dur, Nphot, add_shot_noise, preprocessing_pipeline
+from .QELM import map_angle, AngleEncoding, ReservoirLayer, FiniteStatistics, Reservoir, FactorizedQELM, training
 
 __all__ = ['data_from_h5', 'data_from_txt', 'spectra_normalization', 'add_zeros', 'JWST_interpolation', 'Bl', 
-           'tran_dur', 'Nphot', 'add_shot_noise', 'preprocessing_pipeline']
+           'tran_dur', 'Nphot', 'add_shot_noise', 'preprocessing_pipeline', 'map_angle', 'AngleEncoding', 'ReservoirLayer', 'FiniteStatistics', 'Reservoir', 'FactorizedQELM']
