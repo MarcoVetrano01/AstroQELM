3f863e309e0f60a48fe3e4c7c8f05dffe1d3ad37
Marco <marco.vetrano01@gmail.com>
2025-12-05 12:50:07 +0100

    Imported QELM module in the init file

--- AstroQELM/_init_.py
+++ AstroQELM/_init_.py
@@ -1,5 +1,6 @@
 from .utils import *
 from .preprocessing import *
+from .QELM import *
 
 __all__ = ['data_from_h5', 'data_from_txt', 'spectra_normalization', 'add_zeros', 'JWST_interpolation', 'Bl', 
            'tran_dur', 'Nphot', 'add_shot_noise', 'preprocessing_pipeline']
