eb785cd1a4ef9fe13078f54fc6fa69b61022c76d
Marco <marco.vetrano01@gmail.com>
2025-12-05 16:15:46 +0100

    init file was importing a function from the wrong module

--- AstroQELM/_init_.py
+++ AstroQELM/_init_.py
@@ -1,6 +1,6 @@
-from .utils import data_from_h5, data_from_txt
-from .preprocessing import spectra_normalization, add_zeros, JWST_interpolation, Bl, tran_dur, Nphot, add_shot_noise, preprocessing_pipeline
+from .utils import data_from_h5, data_from_txt, add_zeros
+from .preprocessing import _spectra_normalization, JWST_interpolation, Bl, tran_dur, Nphot, add_shot_noise, preprocessing_pipeline
 from .QELM import map_angle, AngleEncoding, ReservoirLayer, FiniteStatistics, Reservoir, FactorizedQELM, training
 
-__all__ = ['data_from_h5', 'data_from_txt', 'spectra_normalization', 'add_zeros', 'JWST_interpolation', 'Bl', 
-           'tran_dur', 'Nphot', 'add_shot_noise', 'preprocessing_pipeline', 'map_angle', 'AngleEncoding', 'ReservoirLayer', 'FiniteStatistics', 'Reservoir', 'FactorizedQELM']
+__all__ = ['data_from_h5', 'data_from_txt', '_spectra_normalization', 'add_zeros', 'JWST_interpolation', 'Bl', 
+           'tran_dur', 'Nphot', 'add_shot_noise', 'preprocessing_pipeline', 'map_angle', 'AngleEncoding', 'ReservoirLayer', 'FiniteStatistics', 'Reservoir', 'FactorizedQELM', 'training']
