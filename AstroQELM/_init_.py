757b4bf51b36d82b33b1c0c3cb6575ec2f02bc27
Marco <marco.vetrano01@gmail.com>
2025-12-05 16:31:43 +0100

    test

--- AstroQELM/_init_.py
+++ AstroQELM/_init_.py
@@ -3,4 +3,5 @@ from .preprocessing import _spectra_normalization, JWST_interpolation, Bl, tran_
 from .QELM import map_angle, AngleEncoding, ReservoirLayer, FiniteStatistics, Reservoir, FactorizedQELM, training
 
 __all__ = ['data_from_h5', 'data_from_txt', '_spectra_normalization', 'add_zeros', 'JWST_interpolation', 'Bl', 
-           'tran_dur', 'Nphot', 'add_shot_noise', 'preprocessing_pipeline', 'map_angle', 'AngleEncoding', 'ReservoirLayer', 'FiniteStatistics', 'Reservoir', 'FactorizedQELM', 'training']
+           'tran_dur', 'Nphot', 'add_shot_noise', 'preprocessing_pipeline', 'map_angle', 'AngleEncoding', 'ReservoirLayer', 
+           'FiniteStatistics', 'Reservoir', 'FactorizedQELM', 'training']
