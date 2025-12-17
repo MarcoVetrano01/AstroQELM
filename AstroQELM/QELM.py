6370fe28ed64449bdeff44f44c991986de29b46a
Marco <marco.vetrano01@gmail.com>
2025-12-17 16:07:28 +0100

    Added helper functions for Factorized QELM fanction. Added functionality to use a second layer in the qelm in order to predict a first parameter and simplify the training of following models to predict other parameters

--- AstroQELM/QELM.py
+++ AstroQELM/QELM.py
@@ -143,9 +143,12 @@ def training(x_train: np.ndarray, y_train: MatrixLike, regularize: bool = True) 
 
     return model
 
-def FactorizedQELM(data: MatrixLike, targets: MatrixLike, nq: int , global_properties: np.ndarray | None = None, patchwise_properties: np.ndarray | None = None, depth: int = 1, shots: int | MatrixLike = 1024, train_size: float = 0.8, regularize: bool = True, disable_progress_bar: bool = False) -> MatrixLike:
+from typing import Union
+from sklearn.linear_model import Ridge, RidgeCV
 
-    """Factorized QELM Wrapper.
+MatrixLike = Union[np.ndarray, list]
+def _output(data: MatrixLike, nq: int , global_properties: np.ndarray | None = None, patchwise_properties: np.ndarray | None = None, depth: int = 1, shots: int | MatrixLike = 1024, disable_progress_bar: bool = False) -> MatrixLike:
+    """Factorized QELM Wrapper
      Args:
          data (MatrixLike): Input data to be encoded of shape (patches, n_samples, n_features).
          targets (MatrixLike): Target values of shape (n_samples, n_outputs).
@@ -158,30 +161,17 @@ def FactorizedQELM(data: MatrixLike, targets: MatrixLike, nq: int , global_prope
      Returns:
          MatrixLike: Predicted values.
      """
-
-    data = np.array(data)
-    targets = np.array(targets)
-    data_mapped = map_angle(data)
-
+    
     patches = data.shape[0]
     dim = data.shape[1]
     enc_dim = data.shape[2]
-    par = np.random.uniform(0, np.pi, size=(patches, depth,  2 * nq))
-    shots = np.array(shots, dtype = int)
-
+    stat = len(shots)
+    
     if enc_dim > nq:
         raise ValueError("Number of encoded features must be less than or equal to number of qubits.")
-    if np.any(shots < 1):
-        raise ValueError("All shots must be at least 1.")
-    if not (0 < train_size < 1):
-        raise ValueError("Train size must be between 0 and 1.")
     
-    
-    stat = len(shots)
-    split_idx = int(dim * train_size)
-    y_train = targets[:split_idx]
-    y_test = targets[split_idx:]
-    features = y_train.shape[1]
+    data_mapped = map_angle(data)
+    par = np.random.uniform(0, np.pi, size=(patches, depth,  2 * nq))
     
     results_si = np.zeros((patches, 2 ** nq, dim))
     results_sf = np.zeros((patches, 2 ** nq, stat, dim))
@@ -214,19 +204,126 @@ def FactorizedQELM(data: MatrixLike, targets: MatrixLike, nq: int , global_prope
     
     results_sf = np.transpose(results_sf, (1, 2, 0))
 
-    x_train = results_si[:split_idx]
-    x_test = results_si[split_idx:]
-    W_si = training(x_train, y_train, regularize)
-    y_pred_si = W_si.predict(x_test)
+    return results_si, results_sf
+
+def _windows_testing(x: MatrixLike, xtest: MatrixLike, n_windows: int, y_special: MatrixLike, y_train_windows: MatrixLike, regularize: bool, test_size: float, features: int, W_special: Ridge | RidgeCV, special_index: int) -> MatrixLike:
+
+    window = 1 / n_windows
+    slide = window / 4
+    W_windows_si = []
+    negative_outlayers_si = y_special < - slide
+    over_outlayers_si = y_special > 1 + slide
+    for i in range(n_windows):
+        mask = y_special <= (i + 1) * window + slide
+        mask = (y_special >= i * window - slide) * mask
+        if i == 0:
+            mask = mask + negative_outlayers_si
+        if i == n_windows - 2:
+            mask = mask + over_outlayers_si
+        W_windows_si.append(training(x[mask], y_train_windows[mask], regularize))
+    
+    ytest_si = np.zeros((test_size, features - 1))
+    ytest_special_si = np.zeros((test_size))
+    ytest_special_si = W_special.predict(xtest)
+    negative_outlayers_si = ytest_special_si < - slide
+    over_outlayers_si = ytest_special_si > 1 + slide
+    for i in range(n_windows):
+        mask = ytest_special_si <= (i + 1) * window + slide
+        mask = (ytest_special_si >= i * window - slide) * mask
+        if i == 0:
+            mask = mask + negative_outlayers_si
+        if i == n_windows - 2:
+            mask = mask + over_outlayers_si
+        ytest_si[mask] = W_windows_si[i].predict(xtest[mask])
+    ytest_si = np.insert(ytest_si, special_index, ytest_special_si, axis = 1)
+
+    return ytest_si
+
+
+def FactorizedQELM(data: MatrixLike, targets: MatrixLike, nq: int , global_properties: np.ndarray | None = None, patchwise_properties: np.ndarray | None = None, depth: int = 1, shots: int | MatrixLike = 1024, train_size: float = 0.5, train_size_special: float | None = 0.3, n_windows:int | None = 5, special_index: int | None = 6, regularize: bool = True, disable_progress_bar: bool = False) -> MatrixLike:
+
+    """Factorized QELM Wrapper
+     Args:
+         data (MatrixLike): Input data to be encoded of shape (patches, n_samples, n_features).
+         targets (MatrixLike): Target values of shape (n_samples, n_outputs).
+         nq (int): Number of qubits.
+         means (np.ndarray | None): Mean values for data normalization. 
+         depth (int): Depth of the reservoir layer.
+         shots (int): Number of shots (samples) to simulate.
+         train_size (float): Proportion of data to be used for training. Default is 0.8.
+         regularize (bool): Whether to use regularization. Default is True.
+     Returns:
+         MatrixLike: Predicted values.
+     """
+
+    data = np.array(data)
+    targets = np.array(targets)
+    dim = data.shape[1]
+
+    if isinstance(shots, int):
+        shots = np.array([shots], dtype = int)
+    else:
+        shots = np.array(shots, dtype = int)
+        
+    stat = len(shots)
+    split_idx = int(dim * train_size)
+    
+    if train_size_special is None or n_windows is None:
+        n_windows = None
+        train_size_special = None
+        split_idx_special = 0
+        total_train_size = split_idx
+        y_train_special = targets[: total_train_size]
+        a = total_train_size
+        b = dim
+    else: 
+        split_idx_special = int(dim * train_size_special)
+        total_train_size = split_idx_special + split_idx
+        y_train_special = targets[: split_idx_special, special_index].reshape(-1, 1)
+        y_train_windows = np.delete(targets[split_idx_special : total_train_size], special_index, axis = 1)
+        a = split_idx_special
+        b = total_train_size
+    
+    if np.any(shots < 1):
+        raise ValueError("All shots must be at least 1.")
+    if not (0 < total_train_size < dim):
+        raise ValueError("Train size must be between 0 and 1.")
+    
+    features = targets.shape[1]
+
+    results_si, results_sf = _output(data, nq, global_properties, patchwise_properties, depth, shots, disable_progress_bar)
+    W_special_si = training(results_si[: a], y_train_special, regularize)
+    y_special_si = W_special_si.predict(results_si[a : b])
 
     if np.any(shots > 1):
-        y_pred_sf = np.zeros((stat, dim - split_idx, features))
+        try:
+            y_special_sf = np.zeros((stat, b - a, y_special_si.shape[1]))
+        except:
+            y_special_sf = np.zeros((stat, b - a))
+        
         for i in tqdm(range(stat), desc="Finite statistics training", disable = disable_progress_bar):
-            x_train_sf = results_sf[i, :split_idx]
-            x_test_sf = results_sf[i, split_idx:]
-            W_sf = training(x_train_sf, y_train, regularize)
-            y_pred_sf[i] = W_sf.predict(x_test_sf)
+            W_special_sf = training(results_sf[i, : a], y_train_special, regularize)
+            y_special_sf[i] = W_special_sf.predict(results_sf[i, a : b])
     else:
-        y_pred_sf = np.zeros_like(y_pred_si)
+        y_special_sf = np.zeros_like(y_special_si)
     
-    return y_pred_si, y_pred_sf
+    if isinstance(n_windows, int):
+        x = results_si[a : b]      
+        xtest = results_si[total_train_size :]
+        test_size = dim - total_train_size
+        ypred_si = _windows_testing(x, xtest, n_windows, y_special_si, y_train_windows, regularize, test_size, features, W_special_si, special_index)
+        if np.any(shots > 1):
+            ypred_sf = np.zeros((stat, dim - total_train_size, features))
+            for i in tqdm(range(stat), desc="Finite statistics training", disable = disable_progress_bar):
+                x = results_sf[i, a : b]
+                xtest = results_sf[i, total_train_size :]
+
+                ypred_sf[i] = _windows_testing(x, xtest, n_windows, y_special_sf[i], y_train_windows, regularize, test_size, features, W_special_sf, special_index)
+        else:
+            ypred_sf = np.zeros_like(ypred_si)
+    
+    else:
+        ypred_si = y_special_si
+        ypred_sf = y_special_sf
+    
+    return ypred_si, ypred_sf
